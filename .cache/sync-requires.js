53e20629dc865d08a0dd1f75bbecf372fea5f275
shekhar3672 <jadhavshekhar444@gmail.com>
2020-06-01 13:56:11 +0530

    api change

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/.cache/dev-404-page.js"))),
+  "component---src-pages-about-css-modules-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/src/pages/about-css-modules.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/src/pages/blog.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/shekhar/Desktop/data/webApp/src/pages/index.js")))
+}
+
